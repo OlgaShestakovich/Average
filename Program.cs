c23f2bd901354b44cb527b37e84aa5a61af4e041
Olga Shestakovich <olga.shestakovich1@gmail.com>
2020-03-03 21:36:57 +0300

    Add Currency Converter

--- Program.cs
+++ Program.cs
@@ -6,26 +6,28 @@ namespace NewCours
     {
         static void Main(string[] args)
         {
-            //*******************************Arithmetic mean of two numbers
+
+            //****************************************************Arithmetic mean of two numbers
 
             Console.WriteLine("Нахождение среднего арифметического\n");
+           
             Console.WriteLine("Введите первое значение: ");
-            string FirstNumber = Console.ReadLine();
+            string FirstValue = Console.ReadLine();
             Console.WriteLine();
 
             Console.WriteLine("Введите второе значение: ");
-            string SecondNumber = Console.ReadLine();
+            string SecondValue = Console.ReadLine();
             Console.WriteLine();
 
-            double a = Convert.ToDouble(FirstNumber);
-            double b = Convert.ToDouble(SecondNumber);
+            double a = Convert.ToDouble(FirstValue);
+            double b = Convert.ToDouble(SecondValue);
             double result = (a + b) / 2;
             Console.WriteLine("Среднее арифметическое введенных значений = "+ result);
-            Console.WriteLine("Для продолжения нажмите любую клавишу\n");
+            Console.WriteLine("Для продолжения нажмите \"Enter\"\n");
             Console.ReadLine();
             Console.Clear();
 
-            //*******************************Sum and multiplication of numbers
+            //****************************************************Sum and multiplication of numbers
 
             Console.WriteLine("Нахождение суммы и произведения значений\n");
             Console.WriteLine("Введите первое значение\n");
@@ -47,7 +49,20 @@ namespace NewCours
             double ResultMultiplication = (d * e * f);
             Console.WriteLine("Сумма введенных значений= " + ResultSum + "\n ");
             Console.WriteLine("Произведение введенных значений= " + ResultMultiplication + "\n ");
+            Console.WriteLine("Для продолжения нажмите \"Enter\"\n");
+            Console.ReadLine();
+            Console.Clear();
 
+            //**************************************************************** Currency Converter
+
+            double UsdToRub = 2.31;
+            Console.WriteLine("Конвертер валют: курс $/руб =2,31 \n");
+            
+            Console.WriteLine("Введите сумму в $");
+            double SumUsd = double.Parse(Console.ReadLine());
+           
+            Console.WriteLine(SumUsd+"$ ="+(UsdToRub* SumUsd)+" руб.");
+            Console.ReadLine();
 
 
 
