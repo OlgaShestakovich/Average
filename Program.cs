a0d06f98050494731e703139dc0f251543088374
Olga Shestakovich <olga.shestakovich1@gmail.com>
2020-03-03 20:33:19 +0300

    Add arithmetic mean of two numbers

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NewCours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arithmetic mean of two numbers
+
+            Console.WriteLine("Введите первое значение\n");
+            string FirstNumber = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Введите второе значение\n");
+            string SecondNumber = Console.ReadLine();
+            Console.WriteLine();
+
+            double a = Convert.ToDouble(FirstNumber);
+            double b = Convert.ToDouble(SecondNumber);
+            double result = (a + b) / 2;
+            Console.WriteLine("Среднее арифметическое " + a + " и " + b + " = " + result);
+
+        }
+    }
+}
